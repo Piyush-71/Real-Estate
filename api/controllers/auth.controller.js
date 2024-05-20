a61dca954f4da4fe700186c060fbf8268cf62a0f
Piyush-71 <121cs0763@nitrkl.ac.in>
2024-05-20 18:24:27 +0530

    complete delete and signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -61,3 +61,12 @@ export const google = async (req, res, next) => {
         next(error);
     }
 }
+
+export const signOut = (req, res) => {
+    try {
+        res.clearCookie('access_token')
+        res.status(200).json({message: 'user has been signed out successfully'})
+    } catch (error) {
+        next(error);
+    }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import { useRef, useState, useEffect } from "react"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage"
 import { app } from "../firebase"
-import {updateUserStart, updateUserSuccess, updateUserFailure} from "../redux/user/userSlice"
+import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess} from "../redux/user/userSlice"
 import { useDispatch } from "react-redux"
 export default function Profile() {
   const {currentUser, loading, error} = useSelector((state) => state.user)
@@ -71,6 +71,38 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout')
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message))
+        return
+      }
+      dispatch(signOutUserSuccess(data))
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message))
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
     <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -110,8 +142,8 @@ export default function Profile() {
 
     </form>
     <div className="flex justify-between mt-5">
-      <span className="text-red-700 cursor-pointer">Delete account</span>
-      <span className="text-red-700 cursor-pointer">Sign out</span>
+      <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
+      <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
     </div>
 
     <p className="text-red-700 mt-7">{error ? error : ''}</p>
